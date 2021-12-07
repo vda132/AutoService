4e6eac92a602d9b0f2f0c1698963fa7d1bf51c9d
vda_132 <vda_132@ukr.net>
2021-12-07 16:33:44 +0200

    Добавил авторизацию и определение должности.

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -21,9 +21,36 @@ namespace WpfApp1
     /// </summary>
     public partial class MainWindow : Window
     {
+        AutoServiceContext serviceDB = new AutoServiceContext();
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Account acc = new Account();
+            try
+            {
+                acc = serviceDB.Accounts.FirstOrDefault(A => A.LoginAccount == loginTextBox.Text);
+                if (acc == null) throw new ArgumentNullException();
+                if (acc.PasswordAccount == passwordTextBox.Password)
+                {
+                    Manipulation manipulationWindow = new Manipulation(ref acc);
+                    this.Close();
+                    manipulationWindow.Show();
+                }
+                else throw new Exception();
+
+            }
+            catch (ArgumentNullException)
+            {
+                MessageBox.Show("Поля должны быть заполнены.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Пароль неверный.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
         }
     }
 }
---
+++ WpfApp1/Manipulation.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for Manipulation.xaml
+    /// </summary>
+    public partial class Manipulation : Window
+    {
+        Position position;
+        AutoServiceContext autoServiceDB = new AutoServiceContext();
+        private void ShowLogic(Account account)
+        {
+            addingButton.Visibility = Visibility.Visible;
+            Worker worker = autoServiceDB.Workers.FirstOrDefault(A=>A.Idworker==account.Idworker);
+            position = autoServiceDB.Positions.Find(worker.Idposition);
+            switch (position.NamePosition) 
+            {
+                case "Директор":
+                {
+                    addingButton.Content = "Добавить сотрудника";
+                    break;
+                }
+                case "Мастер":
+                {
+                    addingButton.Content = "Добавить клиента";
+                    break;
+                }
+                case "Администратор БД":
+                {
+                    addingButton.Content = "Добавить автоконцерн";
+                    break;
+                }
+            }
+
+        }
+        public Manipulation(ref Account account)
+        {
+            InitializeComponent();
+            ShowLogic(account);
+        }
+
+    }
+}
